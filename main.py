a8ee2b0752b39bae121832179870ba779db7c7d0
Peter Grigor <petrupewdiepie@gmail.com>
2026-06-01 19:51:39 +0300

    md test

--- main.py
+++ main.py
@@ -369,7 +369,9 @@ def get_service_phone_number(country_code):
     hu_number = "+3612550208"
     kr_number = "+82308640190"
 
-    if country_code == "RO":
+    if country_code == "MD":
+        phone_number = ro_number
+    elif country_code == "RO":
         phone_number = ro_number
     elif country_code == "HU":
         phone_number = hu_number
